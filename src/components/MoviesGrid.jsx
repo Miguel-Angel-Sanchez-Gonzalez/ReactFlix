1cb9795f8d1036469cc1449dc239a87b629265c9
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2024-09-06 22:43:06 -0600

    feat: Add movie search functionality

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -3,18 +3,29 @@ import styles from "./MoviesGrid.module.css";
 import { useEffect, useState } from "react";
 import { get } from "../utils/httpClient";
 import { Spinner } from "./Spinner";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 function MoviesGrid() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const query = useQuery();
+  const search = query.get("search");
+
   useEffect(() => {
     setIsLoading(true);
-    get("/discover/movie").then((data) => {
+    const searchUrl = search
+      ? "/search/movie?query=" + search
+      : "/discover/movie";
+    get(searchUrl).then((data) => {
       setMovies(data.results);
       setIsLoading(false);
     });
-  }, []);
+  }, [search]);
 
   if (isLoading) {
     return <Spinner />;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import styles from "./Search.module.css";
 import { GoSearch } from "react-icons/go";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export function Search() {
+  const [searchText, setSearchText] = useState("");
+  const history = useHistory();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    history.push("/?search=" + searchText);
+  };
+
   return (
-    <form className={styles.searchContainer}>
+    <form className={styles.searchContainer} onSubmit={handleSubmit}>
       <div className={styles.searchBox}>
-        <input className={styles.searchInput} placeholder="Search Movie" type="text"></input>
+        <input
+          className={styles.searchInput}
+          placeholder="Search Movie"
+          type="text"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        ></input>
         <button className={styles.searchButton} type="submit">
           <GoSearch size={20} />
         </button>
@@ -13,4 +28,3 @@ export function Search() {
     </form>
   );
 }
- 
