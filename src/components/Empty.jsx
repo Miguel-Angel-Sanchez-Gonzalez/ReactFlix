40110f2b7f34124b1db1df185bf8904d7d568ea6
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2024-09-16 20:29:24 -0600

    feat: Add custom image to empty state component

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -1,5 +1,12 @@
 import styles from "./Empty.module.css";
+import emptyImg from "../empty.png";
 
 export function Empty() {
-  return <div className={styles.empty}>No results</div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <img width={300} height={500} src={emptyImg} alt="empty" />
+      </div>
+    </div>
+  );
 }
